a895e8ff83a126ef725a264a44c01dac5cb81dc5
Matheus <mhqsoftware@gmail.com>
2021-02-14 11:33:12 -0300

    Add loading modal trailer and fix height in mobile

--- src/components/Modals/Trailer.js
+++ src/components/Modals/Trailer.js
@@ -1,30 +1,40 @@
-import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Modal } from "react-bootstrap";
+import Skeleton from "react-loading-skeleton";
+
+import "./styles.css";
 
 function Trailer({ ...props }) {
   const { trailer_id, handler_show_trailer, show_trailer } = props;
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+  }, [show_trailer]);
 
   return (
     <Modal
       show={show_trailer}
       onHide={() => handler_show_trailer(false)}
-      backdrop="static"
       keyboard={false}
       size={"xl"}
+      centered
     >
       <Modal.Header closeButton={() => handler_show_trailer(false)}>
-        <Modal.Title id="contained-modal-title-vcenter">Trailer</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Reproduzir trailer
+        </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <iframe
-          width="100%"
-          height="500px"
-          src={`https://www.youtube.com/embed/${trailer_id}`}
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
-      </Modal.Body>
+      {loading && <Skeleton className="mb-3 p-5" height={500} />}
+      <iframe
+        style={{ border: "none", display: loading ? "none" : "block" }}
+        onLoad={() => setLoading(false)}
+        width="100%"
+        height="500px"
+        src={`https://www.youtube.com/embed/${trailer_id}`}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      ></iframe>
     </Modal>
   );
 }
